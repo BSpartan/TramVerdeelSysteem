3dae9fcb0cd73ac3f8debff24e40a5eaa4db0331
SebastiaanvGaans <sebastiaanvgaans@hotmail.com>
2014-10-07 12:09:56 +0200

    databasemanager toegevoegd

---
+++ TramVerdeelSysteem/TVSLibrary/Database/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+
+namespace TVSLibrary.Database
+{
+    class DatabaseManager
+    {
+        private static OracleConnection connection;
+
+        static DatabaseManager()
+        {
+            //Connects to the database Data source, under the username User Id.
+            connection = new OracleConnection("User Id= ; Password= ; Data Source= //192.168.15.50:1521/fhictora;");
+        }
+
+        static DatabaseManager(string connectionString)
+        {
+            //Connects to the database with the given connectionString.
+            connection = new OracleConnection(connectionString);
+        }
+    }
+}
