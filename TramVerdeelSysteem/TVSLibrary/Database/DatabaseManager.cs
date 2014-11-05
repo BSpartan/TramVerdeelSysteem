395e8acf49b85a3119f5540c783f5a5c4924f828
Bas Mulder <sammu34@hotmail.com>
2014-11-05 10:49:30 +0100

    RFID ADDED
    RFID Toegevoegd

--- TramVerdeelSysteem/TVSLibrary/Database/DatabaseManager.cs
+++ TramVerdeelSysteem/TVSLibrary/Database/DatabaseManager.cs
@@ -11,16 +11,5 @@ namespace TVSLibrary.Database
     {
         private static OracleConnection connection;
 
-        static DatabaseManager()
-        {
-            //Connects to the database Data source, under the username User Id.
-            connection = new OracleConnection("User Id= dbi298273; Password= PeKHcY2bu4; Data Source= //192.168.15.50:1521/fhictora;");
-        }
-
-        static DatabaseManager(string connectionString)
-        {
-            //Connects to the database with the given connectionString.
-            connection = new OracleConnection(connectionString);
-        }
     }
 }
