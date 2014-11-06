8d7293729fec43e8e61ae2ef45e95e329b8ca54a
Brandweerwagen <daan_van_rooij@hotmail.com>
2014-11-06 12:33:08 +0100

    Indeling fixed, no exceptions

--- TramVerdeelSysteem/TVSLibrary/Tram.cs
+++ TramVerdeelSysteem/TVSLibrary/Tram.cs
@@ -40,7 +40,7 @@ namespace TVSLibrary
         {
             this.Tramdriver = tramdriver;
         }
-        public int SortTram(List<Sector> AllSectors)
+        public Sector SortTram(List<Sector> AllSectors)
         {
             bool trackset = false;
             Sector current = null;
@@ -49,46 +49,46 @@ namespace TVSLibrary
                 if (this.Status == Status.Cleaning)
                 {
                 }
-                if (this.Status == Status.Defect)
+                else if (this.Status == Status.Defect)
                 {
                 }
-                if (this.Status == Status.Service)
+                else if (this.Status == Status.Service)
                 {
                 }
-                if (this.TramId <= 841 && this.TramId >= 817)
+                else if (this.TramId <= 841 && this.TramId >= 817)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 30)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 33)
+                        else if (s.Track.Number == 33)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 35)
+                        else if (s.Track.Number == 35)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 57)
+                        else if (s.Track.Number == 57)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -97,31 +97,31 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if ((this.TramId <= 901 && this.TramId >= 920) || (this.TramId <= 2201 && this.TramId >= 2204))
+                else if ((this.TramId <= 901 && this.TramId >= 920) || (this.TramId <= 2201 && this.TramId >= 2204))
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 37)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 54)
+                        else if (s.Track.Number == 54)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 56)
+                        else if (s.Track.Number == 56)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -130,22 +130,22 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if (this.TramId == 2001 || this.TramId == 2004 || this.TramId == 2014 || this.TramId == 2032 || this.TramId == 2037 || this.TramId == 2039 || this.TramId == 2043 || this.TramId == 2050 || this.TramId == 2058 || this.TramId == 2072)
+                else if (this.TramId == 2001 || this.TramId == 2004 || this.TramId == 2014 || this.TramId == 2032 || this.TramId == 2037 || this.TramId == 2039 || this.TramId == 2043 || this.TramId == 2050 || this.TramId == 2058 || this.TramId == 2072)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 32)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 41)
+                        else if (s.Track.Number == 41)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -154,40 +154,40 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if (this.TramId == 2002 || this.TramId == 2008 || this.TramId == 2017 || this.TramId == 2018 || this.TramId == 2019 || this.TramId == 2025 || this.TramId == 2031 || this.TramId == 2036 || this.TramId == 2042 || this.TramId == 2049 || this.TramId == 2051 || this.TramId == 2054 || this.TramId == 2055)
+                else if (this.TramId == 2002 || this.TramId == 2008 || this.TramId == 2017 || this.TramId == 2018 || this.TramId == 2019 || this.TramId == 2025 || this.TramId == 2031 || this.TramId == 2036 || this.TramId == 2042 || this.TramId == 2049 || this.TramId == 2051 || this.TramId == 2054 || this.TramId == 2055)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 34)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 38)
+                        else if (s.Track.Number == 38)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 55)
+                        else if (s.Track.Number == 55)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 63)
+                        else if (s.Track.Number == 63)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -196,22 +196,22 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if (this.TramId == 2003 || this.TramId == 2005 || this.TramId == 2011 || this.TramId == 2012 || this.TramId == 2013 || this.TramId == 2016 || this.TramId == 2020 || this.TramId == 2022 || this.TramId == 2026 || this.TramId == 2027 || this.TramId == 2028 || this.TramId == 2038 || this.TramId == 2044 || this.TramId == 2047 || this.TramId == 2060 || this.TramId == 2071)
+                else if (this.TramId == 2003 || this.TramId == 2005 || this.TramId == 2011 || this.TramId == 2012 || this.TramId == 2013 || this.TramId == 2016 || this.TramId == 2020 || this.TramId == 2022 || this.TramId == 2026 || this.TramId == 2027 || this.TramId == 2028 || this.TramId == 2038 || this.TramId == 2044 || this.TramId == 2047 || this.TramId == 2060 || this.TramId == 2071)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 45)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 52)
+                        else if (s.Track.Number == 52)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -220,22 +220,22 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if (this.TramId == 2006 || this.TramId == 2010 || this.TramId == 2015 || this.TramId == 2021 || this.TramId == 2030 || this.TramId == 2040 || this.TramId == 2045 || this.TramId == 2046 || this.TramId == 2048 || this.TramId == 2056 || this.TramId == 2067 || this.TramId == 2070)
+                else if (this.TramId == 2006 || this.TramId == 2010 || this.TramId == 2015 || this.TramId == 2021 || this.TramId == 2030 || this.TramId == 2040 || this.TramId == 2045 || this.TramId == 2046 || this.TramId == 2048 || this.TramId == 2056 || this.TramId == 2067 || this.TramId == 2070)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 44)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 53)
+                        else if (s.Track.Number == 53)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -244,22 +244,22 @@ namespace TVSLibrary
                         }
                     }
                 }
-                if (this.TramId == 2007 || this.TramId == 2009 || this.TramId == 2023 || this.TramId == 2024 || this.TramId == 2029 || this.TramId == 2033 || this.TramId == 2034 || this.TramId == 2035 || this.TramId == 2041 || this.TramId == 2052 || this.TramId == 2053 || this.TramId == 2059 || this.TramId == 2061 || this.TramId == 2064 || this.TramId == 2066 || this.TramId == 2068)
+                else if (this.TramId == 2007 || this.TramId == 2009 || this.TramId == 2023 || this.TramId == 2024 || this.TramId == 2029 || this.TramId == 2033 || this.TramId == 2034 || this.TramId == 2035 || this.TramId == 2041 || this.TramId == 2052 || this.TramId == 2053 || this.TramId == 2059 || this.TramId == 2061 || this.TramId == 2064 || this.TramId == 2066 || this.TramId == 2068)
                 {
                     foreach (Sector s in AllSectors)
                     {
                         if (s.Track.Number == 36)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
                                 break;
                             }
                         }
-                        if (s.Track.Number == 43)
+                        else if (s.Track.Number == 43)
                         {
-                            if (s.Available == true)
+                            if (s.Blocked == false && s.Tram == null)
                             {
                                 current = s;
                                 trackset = true;
@@ -268,9 +268,21 @@ namespace TVSLibrary
                         }
                     }
                 }
+                else
+                {
+                    foreach (Sector s in AllSectors)
+                    {
+                        if (s.Blocked == false && s.Tram == null)
+                        {
+                            current = s;
+                            trackset = true;
+                            break;
+                        }
+                    }
+                }
             }
             this.Sector = current;
-            return Sector.ID;
+            return current;
         }
     }
 }
